67869b01d13d91a36142f1be269bf3048f8f30b0
Martyn Loughran <me@mloughran.com>
2009-03-30 23:36:55 +0100

    Clean up code formatting

--- lib/api_cache/api.rb
+++ lib/api_cache/api.rb
@@ -1,17 +1,19 @@
 require 'net/http'
 
-# This class wraps up querying the API and remembers when each API was 
+# This class wraps up querying the API and remembers when each API was
 # last queried in case there is a limit to the number that can be made.
+#
 class APICache::API
   def initialize
     @query_times = {}
   end
-  
+
   # Checks whether the API can be queried (i.e. whether retry_time has passed
   # since the last query to the API).
-  # 
+  #
   # If retry_time is 0 then there is no limit on how frequently queries can
   # be made to the API.
+  #
   def queryable?(key, retry_time)
     if @query_times[key]
       if Time.now - @query_times[key] > retry_time
@@ -26,15 +28,16 @@ class APICache::API
       true
     end
   end
-  
+
   # Fetch data from the API.
-  # 
+  #
   # If no block is given then the key is assumed to be a URL and which will
   # be queried expecting a 200 response. Otherwise the return value of the
   # block will be used.
-  # 
+  #
   # If the block is unable to fetch the value from the API it should raise
   # APICache::Invalid.
+  #
   def get(key, timeout, &block)
     APICache.logger.log "Fetching data from the API"
     @query_times[key] = Time.now
@@ -49,9 +52,9 @@ class APICache::API
   rescue Timeout::Error, APICache::Invalid => e
     raise APICache::CannotFetch, e.message
   end
-  
-private
-  
+
+  private
+
   def get_via_http(key, timeout)
     response = redirecting_get(key)
     case response
--- lib/api_cache/cache.rb
+++ lib/api_cache/cache.rb
@@ -1,18 +1,22 @@
+require 'digest/md5'
+
 # Cache performs calculations relating to the status of items stored in the
 # cache and delegates storage to the various cache stores.
-require 'digest/md5'
+#
 class APICache::Cache
   attr_accessor :store
+
   def initialize(store)
     @store = store.send(:new)
   end
-  
+
   # Returns one of the following options depending on the state of the key:
-  # 
+  #
   # * :current (key has been set recently)
   # * :refetch (data should be refetched but is still available for use)
   # * :invalid (data is too old to be useful)
   # * :missing (do data for this key)
+  #
   def state(key, refetch_time, invalid_time)
     if @store.exists?(encode(key))
       if !@store.expired?(encode(key), refetch_time)
@@ -26,15 +30,16 @@ class APICache::Cache
       :missing
     end
   end
-  
+
   def get(key)
     @store.get(encode(key))
   end
-  
+
   def set(key, value)
     @store.set(encode(key), value)
     true
   end
+
   def encode(key)
     Digest::MD5.hexdigest key
   end
