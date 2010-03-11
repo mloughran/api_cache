bb3f2230f1008b5a6b96f6b5488c46af71dcf667
David Cuddeback <kauschovar@gmail.com>
2010-03-11 17:12:02 +0800

    added NullStore class for times when caching is undesirable

---
+++ lib/api_cache/null_store.rb
@@ -0,0 +1,20 @@
+class APICache
+  # A null store for environments where caching may be undesirable, such as
+  # testing.
+  class NullStore < APICache::AbstractStore
+    def initialize
+    end
+    
+    def exists?(key)
+      false
+    end
+
+    def set(key, value)
+      true
+    end
+
+    def expired?(key, timeout)
+      true
+    end
+  end
+end
---
+++ spec/null_store_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe APICache::NullStore do
+  before :each do
+    @store = APICache::NullStore.new
+  end
+
+  it "should NOT set" do
+    @store.exists?('foo').should be_false
+    @store.set('foo', 'bar')
+    @store.exists?('foo').should be_false
+  end
+
+  it "should allows say keys are expired" do
+    @store.expired?('foo', 1).should be_true
+    @store.set('foo', 'bar')
+    @store.expired?('foo', 1).should be_true
+  end
+end
