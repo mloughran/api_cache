dffc93456a61ca9cee2797b1e767268f8af53542
Martyn Loughran <me@mloughran.com>
2011-03-25 01:32:06 +0000

    Fix specs so that they run with ruby 1.9.2 & bundler

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require 'spec_helper'
 
 describe "api_cache" do
   before :each do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
-$:.push File.join(File.dirname(__FILE__), '..', 'lib')
-require "rubygems"
-require "api_cache"
 require "spec"
 require "fakeweb"
+
+$:.push File.join(File.dirname(__FILE__), '..', 'lib')
+require "api_cache"
 
 APICache.logger.level = Logger::FATAL
